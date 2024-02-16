d862b624b7d8f32be6d775572a94c52892a98f4d
bkhazaal <thajukez101@gmail.com>
2024-02-16 16:40:24 -0700

    stillnotworking

--- TicTac.js
+++ TicTac.js
@@ -1,14 +1,56 @@
-const one = document.getElementById("square1");
-const two = document.getElementById("square2");
-const three = document.getElementById("square3");
-const four = document.getElementById("square4");
-const five = document.getElementById("square5");
-const six = document.getElementById("square6");
-const seven = document.getElementById("square7");
-const eight = document.getElementById("square8");
-const nine = document.getElementById("square9");
-const placedTiles = [];
+const cells = document.querySelectorAll(".buttons");
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector(".restart");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+let options = ["", "", "", "", "", "", "", "", "",];
+let currentPlayer = "X";
+let running = false;
 
-function Test() {
-    console.log("it is working");
+initalizeGame();
+
+function initalizeGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartButton.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+    if(options[cellIndex] != "" || !running) {
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index) {
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+    changePlayer();
+
+}
+
+function changePlayer() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+
 }
