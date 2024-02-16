ca5b4cfc7915e219d108c0e4ae24a34622747347
bkhazaal <thajukez101@gmail.com>
2024-02-16 16:05:38 -0700

    javascript

---
+++ TicTac.js
@@ -0,0 +1,14 @@
+const one = document.getElementById("square1");
+const two = document.getElementById("square2");
+const three = document.getElementById("square3");
+const four = document.getElementById("square4");
+const five = document.getElementById("square5");
+const six = document.getElementById("square6");
+const seven = document.getElementById("square7");
+const eight = document.getElementById("square8");
+const nine = document.getElementById("square9");
+const placedTiles = [];
+
+function Test() {
+    console.log("it is working");
+}
